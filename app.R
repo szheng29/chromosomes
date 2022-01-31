dbef4e667cddb5bf0b2f951a61c6c973d868e77b
Yuan Lab <yuanlab@Yuan-Lab.local>
2022-01-31 10:13:26 -0500

    create app

---
+++ app.R
@@ -0,0 +1,44 @@
+#' Plot three-dimensional chromosomes
+#'
+#' @param coords 
+#'
+#' @return A `rgl` interactive three-dimensional plot
+#' @export
+#'
+#' @examples
+#' library(shiny)
+#' library(rgl)
+#' teta = seq(0, 3*pi, len = 1000)
+#' df1 <- data.frame(x = seq(1,2,len = 1000),
+#'                   y = cos(teta),
+#'                   z = cos(teta)*sin(teta),
+#'                   chr = rep(1,1000))
+#' df2 <- data.frame(x = cos(teta)*sin(teta),
+#'                   y = seq(0,2,leng = 1000),
+#'                   z = cos(teta),
+#'                   chr = rep(2,1000))
+#' df3 <- rbind(df1,df2)
+#' plot3dChromosomes(df3)
+
+plot3dChromosomes <- function(coords) {
+    app = shinyApp(
+        ui <- bootstrapPage(
+            rglwidgetOutput("rglPlot")
+        ),
+        
+        server <- function(input, output) {
+            output$rglPlot <- renderRglwidget({
+                try(close3d(), silent = TRUE)
+                bg3d(col = "white")
+                with(coords, plot3d(x, y, z, 
+                                    type = "s",
+                                    col = as.factor(chr)))
+                axes3d()
+                rglwidget()
+            })
+            
+        }
+        
+    )
+    runApp(app)
+}
